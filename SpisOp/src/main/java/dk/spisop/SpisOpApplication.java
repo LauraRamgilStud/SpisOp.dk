e2a6a252bc44d81081490d407bf67b6402c4210d
LauraRamgilStud <118348928+LauraRamgilStud@users.noreply.github.com>
2023-02-24 16:40:45 +0100

    Add files via upload

---
+++ SpisOp/src/main/java/dk/spisop/SpisOpApplication.java
@@ -0,0 +1,13 @@
+package dk.spisop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpisOpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpisOpApplication.class, args);
+    }
+
+}
---
+++ SpisOp/src/test/java/dk/spisop/SpisOpApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.spisop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpisOpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
