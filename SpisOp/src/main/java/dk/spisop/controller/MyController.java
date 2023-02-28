346b2f202d62f919db0f56962da87205cfa6de76
LauraRamgi <118348928+LauraRamgilStud@users.noreply.github.com>
2023-02-28 17:18:43 +0100

    header-navbar-window

--- SpisOp/src/main/java/dk/spisop/controller/MyController.java
+++ SpisOp/src/main/java/dk/spisop/controller/MyController.java
@@ -5,9 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
 
-    @GetMapping("/Home")
+    @GetMapping("/")
     public String home(){
         return "index";
     }
+
+    @GetMapping("/om-os")
+    public String omOs() {return "omOs";}
+
+    @GetMapping("/kontakt")
+    public String kontakt() {return "kontakt";}
+
+    @GetMapping("/tilmeld")
+    public String tilmeld() {return "tilmeld";}
+
+    @GetMapping("/madspild-i-danmark")
+    public String madspildIDanmark() {return "madspild-i-danmark";}
 
 }
